d0b243b6e7a592939fb8cf48a98e8cd5705ff0d6
Gustavo Mamede <gustavo@vestelivre.com.br>
2025-06-20 04:03:20 -0300

    Conectar banco Neon via variável de ambiente

--- controlerotas/__init__.py
+++ controlerotas/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,9 +7,10 @@ app = Flask(__name__)
 
 app.secret_key = 'a1f4b29e3c8d7f015a96b3de4c72f981'
 app.config['SECRET_KEY'] = 'e9c7a18f4b23d5068fa1b3ce9274dc50'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dadosrota.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 database = SQLAlchemy(app)
 
