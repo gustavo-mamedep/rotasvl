4354096b67c21c27949ce991299d5e5cb4c7dc28
Gustavo Mamede <gustavo@vestelivre.com.br>
2025-12-31 03:38:29 -0300

    correçaoURI

--- controlerotas/__init__.py
+++ controlerotas/__init__.py
@@ -8,10 +8,11 @@ app = Flask(__name__)
 app.secret_key = 'a1f4b29e3c8d7f015a96b3de4c72f981'
 app.config['SECRET_KEY'] = 'e9c7a18f4b23d5068fa1b3ce9274dc50'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-    'DATABASE_URL'
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.getenv('DATABASE_URL')
+    or os.getenv('SQLALCHEMY_DATABASE_URI')
+    or 'postgresql://rotas_user:MAMEDE3276@localhost:5432/rotas_db'
 )
-
 
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
